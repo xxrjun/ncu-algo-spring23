19b4c80ff7b858926b11b1414ec6c1fd86c4fb5b
rjun <rjun0729@gmail.com>
2023-03-30 01:37:07 +0800

    done

--- oj-problems/05/5-1_add_the_page.cpp
+++ oj-problems/05/5-1_add_the_page.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
+#include <array>
 
 using namespace std;
 
@@ -7,68 +8,47 @@ int main()
 {
     int n;
     cin >> n;
-    while (n--)
+
+    vector<array<long long, 2>> v;
+    long long sum = 0;
+    for (int i = 0; i < n; i++)
     {
-        int sum;
         cin >> sum;
-        int pages = ceil((sqrt(1 + 8 * (sum + 1)) - 1) / 2);
-        int missing_page = (pages * (pages + 1)) / 2 - sum;
-        cout << missing_page << " " << pages << endl;
+
+        if (sum <= 0)
+        {
+            v.push_back({0, 0});
+            continue;
+        }
+
+        long long missing_page = 1;
+        long long total_page = 1;
+        long long tmp = total_page;
+
+        while (tmp < sum)
+        {
+            total_page++;
+            tmp += total_page;
+        }
+
+        if (tmp == sum)
+        {
+            total_page++;
+            missing_page = total_page;
+        }
+        else
+        {
+            missing_page = tmp - sum;
+        }
+
+        v.push_back({missing_page, total_page});
     }
-    return 0;
+
+    int len = v.size();
+    for (int i = 0; i < len - 1; i++)
+    {
+        cout << v[i][1] << " " << v[i][0] << endl;
+    }
+
+    cout << v[len - 1][1] << " " << v[len - 1][0];
 }
-
-// #include <iostream>
-// #include <vector>
-// #include <array>
-
-// using namespace std;
-
-// int main()
-// {
-//     int n;
-//     cin >> n;
-
-//     vector<array<long long, 2>> v;
-//     long long sum = 0;
-//     for (int i = 0; i < n; i++)
-//     {
-//         cin >> sum;
-
-//         if (sum <= 0)
-//         {
-//             v.push_back({0, 0});
-//             continue;
-//         }
-
-//         long long missing_page = 1;
-//         long long total_page = 1;
-//         long long tmp = total_page;
-
-//         while (tmp < sum)
-//         {
-//             total_page++;
-//             tmp += total_page;
-//         }
-
-//         if (tmp == sum)
-//         {
-//             total_page++;
-//             missing_page = total_page;
-//         }
-//         else
-//         {
-//             missing_page = tmp - sum;
-//         }
-
-//         v.push_back({missing_page, total_page});
-//     }
-
-//     int len = v.size();
-//     for (int i = 0; i < len - 1; i++)
-//     {
-//         cout << v[i][0] << " " << v[i][1] << endl;
-//     }
-
-//     cout << v[len - 1][0] << " " << v[len - 1][1];
-// }
